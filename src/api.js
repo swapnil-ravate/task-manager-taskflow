d3ff0c3c29727eab43b1aec4d0e5640fde5f2818
Swapnil <swwapnilrvte@gmail.com>
2026-03-12 17:44:27 +0530

    fix: update API_URL to Render backend production URL

--- src/api.js
+++ src/api.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const API_URL = 'http://localhost:5000/api';
+const API_URL = 'https://task-manager-backend-4yrn.onrender.com/api';
 
 // Create axios instance
 const api = axios.create({
